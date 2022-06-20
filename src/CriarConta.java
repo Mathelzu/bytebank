e79e43dd942b8cce8486dc18d53658407c833eb9
Matheus <matheusprogramao2003@gmail.com>
2022-06-20 14:50:30 -0400

    primeira commit

---
+++ src/CriarConta.java
@@ -0,0 +1,21 @@
+public class CriarConta {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        primeiraConta.saldo += 100;
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+        segundaConta.agencia = 146;
+
+        System.out.println("primeira conta tem " + primeiraConta.saldo);
+        System.out.println(primeiraConta.agencia);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+        System.out.println(segundaConta.agencia);
+        System.out.println(segundaConta.numero);
+
+        System.out.println(primeiraConta);
+        System.out.println(segundaConta);
+
+    }
+}
